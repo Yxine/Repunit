b7c0f506f41d5933eeb2da059f7c6e7d344c8ed4
Alexsandr Larin <Yxine@users.noreply.github.com>
2016-08-09 10:25:38 +0700

    x32 build

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8e0ed35e-90dc-4971-ad48-d8e2dbbf1c1b")]
-[assembly: AssemblyVersion("2016.8.7.*")]
-[assembly: AssemblyFileVersion("2016.8.7.461")]
+[assembly: AssemblyVersion("2016.8.9.*")]
+[assembly: AssemblyFileVersion("2016.8.9.461")]
